443dc1303ffd18412eac7ced05ef047fdb53cc76
conradrenno <conradrenno@hotmail.com>
2025-02-09 11:15:47 -0300

    Adding delete method

--- src/main/java/com/devrenno/BackendSpringBoot/services/ClientService.java
+++ src/main/java/com/devrenno/BackendSpringBoot/services/ClientService.java
@@ -44,6 +44,11 @@ public class ClientService {
         return new ClientDTO(client);
     }
 
+    @Transactional
+    public void delete(Long id){
+        repository.deleteById(id);
+    }
+
     public void copyDtoToEntity(ClientDTO clientDTO, Client client){
 
         client.setName(clientDTO.getName());
