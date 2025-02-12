dce6e4663296c3c97f94769556281f3becee6d66
conradrenno <conradrenno@hotmail.com>
2025-02-11 21:40:03 -0300

    Fixing a Transactional parameter mistake

--- src/main/java/com/devrenno/BackendSpringBoot/services/ClientService.java
+++ src/main/java/com/devrenno/BackendSpringBoot/services/ClientService.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
@@ -45,7 +44,7 @@ public class ClientService {
             return new ClientDTO(client);
     }
 
-    @Transactional(propagation = Propagation.NESTED)
+    @Transactional
     public ClientDTO update(Long id, ClientDTO clientDTO){
 
         try {
